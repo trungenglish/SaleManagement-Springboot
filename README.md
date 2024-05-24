86cec6eab05d48c3fc07081a93d101949bdad1c6
trungenglish <nguyentrung23232323@gmail.com>
2024-05-25 00:47:06 +0700

    create project, add bootstrap, config gitignore,updating README.md

---
+++ README.md
@@ -0,0 +1,10 @@
+# Sale Management Project
+## Frameworks and Libraries
+- **Framework**: Spring Boot
+- **Dependencies**
+  - Spring Data JPA
+  - Thymeleaf
+  - Spring Web
+  - Spring Boot DevTools
+  - MySQL Connector/J
+- **Libraries**: Lombok
---
+++ src/main/java/com/example/SaleManagement/SaleManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.SaleManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaleManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaleManagementApplication.class, args);
+	}
+
+}
