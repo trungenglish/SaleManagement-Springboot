749ca222662c2950d4a27f9f6ade04504264630a
trungenglish <nguyentrung23232323@gmail.com>
2024-06-20 22:53:27 +0700

    config spring security to use customize layout login

--- src/main/java/com/example/SaleManagement/config/SecurityConfig.java
+++ src/main/java/com/example/SaleManagement/config/SecurityConfig.java
@@ -1,7 +1,40 @@
 package com.example.SaleManagement.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        return http
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .permitAll())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/images/**","/css/**","/js/**").permitAll()
+                        .anyRequest().authenticated())
+                .build();
+    }
+
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        UserDetails userDetails = User.withDefaultPasswordEncoder()
+//                .username("user")
+//                .password("password")
+//                .roles("USER")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(userDetails);
+//    }
 }
--- src/main/java/com/example/SaleManagement/controller/TestController.java
+++ src/main/java/com/example/SaleManagement/controller/TestController.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class TestController {
-    @GetMapping("/")
-    public String index(){
-        return "/fragments/NavLeftSide";
+    @GetMapping("/login")
+    public String login(){
+        return "login";
     }
 
 }
