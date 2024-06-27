602dbd0cdf44d88b546c9b96f646e6f5717387df
trungenglish <143821682+trungenglish@users.noreply.github.com>
2024-06-27 11:07:47 +0700

    Update AuthController.java

--- src/main/java/com/example/SaleManagement/controller/AuthController.java
+++ src/main/java/com/example/SaleManagement/controller/AuthController.java
@@ -1,11 +1,5 @@
 package com.example.SaleManagement.controller;
 
-import com.example.SaleManagement.dto.LoginDto;
-import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
